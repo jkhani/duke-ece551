05cfbe43f0ece26e3c350c6d31d52574ed82f863
jmk88 <jmk88@duke.edu>
2018-02-02 15:15:11 -0500

    Add maxSeq.c file which contains function for determining the maximum length of increasing numbers in an array, test-subseq.c provides test cases for validating maxSeq function, Makefile compiles the c code and generates the .o files

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n){
+  // corner case for NULL array
+  if (array == NULL){
+    return 0;
+  }
+  else{
+    // variable for holding current length of increasing numbers
+    size_t temp_seq = 1;
+    // variable for holding largest number of increasing numbers in a row
+    size_t seq_max = temp_seq;
+    for (int i = 1; i < n; i++){
+      // check if next number in array is larger than the last
+      if (array[i] > array[i-1]){
+	// if so, add to the sequence
+	temp_seq += 1;
+	if (temp_seq > seq_max){
+	  seq_max = temp_seq;
+	}
+      }
+      else{
+	temp_seq = 1;
+      }
+    }
+    return seq_max;
+  }
+}
