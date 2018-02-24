581d54e0bede437deb198e4d950d37e4eb76748b
jmk88 <jmk88@duke.edu>
2018-02-23 22:44:57 -0500

    Adding test conditions for intersectionArea to return default values

--- 068_circle/circle.cpp
+++ 068_circle/circle.cpp
@@ -10,9 +10,22 @@ void Circle::move(double dx, double dy){
 double Circle::intersectionArea(const Circle & otherCircle){
   double R = otherCircle.radius;
   double dist = center.distanceFrom(otherCircle.center);
+  printf("d = %g",dist);
+  
+  if (dist > (radius+R)){
+    return 0;
+  }
+
+  if ((dist < abs(radius - R)) || (dist == 0)){
+    return 4*3.1415926535897;
+  }
+  
   double area1 = radius*radius*acos(((dist*dist)+(radius*radius)-(R*R))/(2*dist*radius));
+  printf("area1 = %g ",area1);
   double area2 = R*R*acos(((dist*dist)+(R*R)-(radius*radius))/(2*dist*R));
+  printf("area2 = %g ",area2);
   double area3 = pow(((-dist+radius+R)*(dist+radius-R)*(dist-radius+R)*(dist+radius+R)),.5);
+  printf("area3 = %g\n",area3);
 
   return area1+area2-((1/2)*area3);
 }
