d837c38a39017c75cad17c8b51a1452a5a45ca6d
jmk88 <jmk88@duke.edu>
2018-02-25 08:43:00 -0500

    Change intersectionArea() algorithm

--- 068_circle/circle.cpp
+++ 068_circle/circle.cpp
@@ -12,17 +12,17 @@ double Circle::intersectionArea(const Circle & otherCircle){
   double r = radius;
   double d = center.distanceFrom(otherCircle.center);
   
-  if (d > (r+R)){
+  if (d >= (r+R)){
     return 0;
   }
 
-  if ((d < abs(r - R)) || (d == 0)){
+  if (d <= abs(r-R)){
     if (R > r){
       R = r;
     }
     return R*R*3.1415926535;
   }
-  
+
   double a = r*r;
   double b = R*R;
 
@@ -30,5 +30,5 @@ double Circle::intersectionArea(const Circle & otherCircle){
   double z = x*x;
   double y = sqrt(a - z);
 
-  return a*asin(y/r) + b * asin(y/R) - y * (x+sqrt(z+b-a));
+  return a*atan2(y,x) + b *atan2(y,(d-x)) - d*y;
 }
