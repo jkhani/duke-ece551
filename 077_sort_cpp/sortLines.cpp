941b0ac3ef2891374ec24ce20430584d9adc20cc
jmk88 <jmk88@duke.edu>
2018-03-05 11:53:41 -0500

    Code for sortLines in cpp and Makefile

---
+++ 077_sort_cpp/sortLines.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main(int argc, char ** argv) {
+    std::string str;
+    std::vector<std::string> str_vec;
+
+  // If no input file provided, use standard input
+  if (argc ==1) {
+    if(std::cin.good()){
+      while(std::getline(std::cin,str) && !std::cin.eof()){
+	str_vec.push_back(str);
+      }
+      
+      std::sort(str_vec.begin(), str_vec.end());
+
+      for (std::vector<std::string>::const_iterator i = str_vec.begin(); i != str_vec.end(); ++i){
+	std::cout << *i << "\n";
+      }
+    }
+  }
+  else {
+    for( int i=1; i<argc; ++i){
+      std::ifstream ifs (argv[i]);
+
+      if (ifs.is_open()){
+	while(std::getline(ifs,str) && !ifs.eof()){
+	  str_vec.push_back(str);
+	}
+      }
+      else {
+	std::cout << "Error opening file\n";
+	return EXIT_FAILURE;
+      }
+
+      std::sort(str_vec.begin(),str_vec.end());
+      for (std::vector<std::string>::const_iterator i = str_vec.begin(); i != str_vec.end(); ++i){
+	std::cout << *i << "\n";
+      }
+    }
+  }     
+  
+  return EXIT_SUCCESS;
+}
