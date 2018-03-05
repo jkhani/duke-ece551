419526b1bb3e956e27f0b17d7462946a3f6d442d
jmk88 <jmk88@duke.edu>
2018-03-05 16:09:35 -0500

    New attempt for Matrix.h

--- 078_matrix_tmpl/Matrix.h
+++ 078_matrix_tmpl/Matrix.h
@@ -1,128 +1,109 @@
 #ifndef __T_MATRIX_H___
 #define __T_MATRIX_H___
-
 #include <assert.h>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 using namespace std;
-
-
+//YOUR CODE GOES HERE!
 template<typename T>
-class Matrix{
+class Matrix
+{
  private:
   int numRows;
   int numColumns;
   vector< vector<T> > rows;
  public:
- Matrix(): numRows(0), numColumns(0) {
- }
- Matrix(int r, int c): numRows(r), numColumns(c){
-    for (int i=0; i<r; i++){
-      rows[i] = new vector<T>(c);
+  //default constructor
+ Matrix():numRows(0),numColumns(0){}
+ Matrix(int r, int c):numRows(r), numColumns(c) {
+    assert(numRows >= 0 && numColumns >=0);
+    for (int i = 0; i < numRows; i++){
+      rows.push_back(vector<T>(numColumns));
     }
   }
- Matrix(const Matrix & rhs): numRows(rhs.numRows), numColumns(rhs.numColumns), rows(new vector<T>*[rhs.numRows]){
-    for (int i=0; i<numRows; i++){
-      vector<T>* tempArr = new vector<T>(*rhs.rows[i]);
-      rows[i] = tempArr;
+  //copy constructor
+ Matrix(const Matrix & rhs):numRows(rhs.getRows()),numColumns(rhs.getColumns()){
+    for (int i = 0; i < numRows; i++){
+      rows.push_back(rhs[i]);
     }
   }
- ~Matrix() {
-    for (int i=0; i<numRows; i++){
-      delete rows[i];
+  //destructor
+  ~Matrix() {
+    for (int i = 0; i < numRows; i++){
+      rows[i].clear();
     }
-
-    delete[]rows;
+    rows.clear();
   }
-  Matrix & operator=(const Matrix & rhs){
-    if(this != &rhs){
-      vector<T> ** temp = new vector<T>*[rhs.numRows];
-
-      for (int i=0; i<rhs.numRows; i++){
-	vector<T>* tempArr = new vector<T>(*rhs.rows[i]);
-	temp[i] = tempArr;
-      }
-      delete[]rows;
-
-      numRows = rhs.numRows;
-      numColumns = rhs.numColumns;
-
-      rows = temp;
+  //assigment operator
+  Matrix & operator=(const Matrix & rhs) {
+    if(&rhs == this){ return *this;}
+    for (int i = 0; i <numRows; i++){
+      rows[i].clear();
     }
-
+    rows.clear();
+    numRows = rhs.getRows();
+    numColumns = rhs.getColumns();
+    for (int i = 0; i < numRows; i++){
+      rows.push_back(rhs[i]);
+    }
     return *this;
-  }
-
-  int getRows() const {
-    return numRows;
-  }
-
-  int getColumns() const {
-    return numColumns;
-  }
-
-  const vector<T> & operator[](int index) const {
-    assert(index >= 0 && index < numRows);
-    return (*rows[index]);
-  }
-
-  vector<T> & operator[](int index) {
-    assert(index >= 0 && index < numRows);
-    return (*rows[index]);
-  }
-
-  bool operator==(const Matrix & rhs) const {
-    if (numRows != rhs.numRows || numColumns != rhs.numColumns){
-      return false;
+    int getRows() const { return numRows;}
+    int getColumns() const { return numColumns;}
+    const vector<T>  & operator[](int index) const {
+      assert(index >= 0 && index <= numRows-1);
+      return rows[index];
     }
-
-    for (int i=0; i < numRows; i++){
-      if ((*rows[i]) != (*rhs.rows[i])){
-	return false;
-      }
+    vector<T> & operator[](int index){
+      assert(index >= 0 && index <= numRows-1);
+      return rows[index];
     }
-    return true;
-  }
-
-  Matrix operator+(const Matrix & rhs) const{
-    assert(numRows == rhs.numRows && numColumns == rhs.numColumns);
-
-    Matrix sumMatrix(numRows, numColumns);
-
-    for (int i=0; i < numRows; i++){
-      for (int j=0; j < numColumns; j++){
-	(*sumMatrix.rows[i])[j] = (*rows[i])[j]+(*rhs.rows[i])[j];
+    bool operator==(const Matrix & rhs) const {
+      if(&rhs == this){ return 1;}
+      if (numRows != rhs.getRows() || numColumns != rhs.getColumns()){
+	return 0;
       }
+      else{
+	for(int i = 0; i < numRows; i++){
+	  if (rows[i] != (rhs[i])){
+	    return 0;
+	  }
+	}
+      }
+      return 1;
     }
-
-    return sumMatrix;
-  }
-
-};
-
-template<typename S>
-std::ostream & operator<<(std::ostream & s, const Matrix<S> & rhs) {
-  s << "[ ";
-  for (int i=0; i < rhs.getRows(); i++){
-    s << "{";
-    for (int j = 0; j <rhs.getColumns(); j++){
-      s << rhs[i][j];
-      if (j < (rhs.getColumns()-1)){
-	s << ", ";
+    Matrix operator+(const Matrix & rhs) const {
+      assert( (numRows == rhs.getRows()) && (numColumns == rhs.getColumns()));
+      Matrix matrix(*this);
+      for (int i = 0; i < numRows; i ++){
+	for (int j = 0; j < numColumns; j++){
+	  matrix[i][j] += rhs[i][j];
+	}
       }
+      return matrix;
     }
-    s << "}";
-    if (i < (rhs.getRows() -1)){
-	s << ",\n";
+  };
+  template<typename S>
+    std::ostream & operator<<(std::ostream & s, const Matrix<S> & rhs) {
+    if( rhs.getRows() == 0 ){
+      s <<"[  ]";
+    }
+    else
+      {
+	std::cout << "[ {";
+	for (int i = 0; i < rhs.getRows(); i++){
+	  for (int j = 0; j < rhs.getColumns(); j++){
+	    s << rhs[i][j];
+	    if (j < rhs.getColumns()-1){
+	      s<<", ";
+	    }
+	  }
+	  if (i < rhs.getRows()-1){
+	    s << "},\n{";
+	  }
+	}
+	s<<"} ]";
       }
+    return s;
   }
-
-  s << " ]";
-
-  return s;
-}
-
-  
-
-#endif
+  #endif
